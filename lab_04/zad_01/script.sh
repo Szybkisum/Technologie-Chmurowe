394f144ed9dc8242a80a56993a4470a2bb542656
Mariusz Turzyński <szybkisuuuuuum06@gmail.com>
2025-03-25 20:51:59 +0100

    lab_04

---
+++ lab_04/zad_01/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NGINX_CONTAINER="my-nginx-container"
+VOLUME="nginx_data"
+
+info() {
+    echo -e "\n\033[1;34m[$1]\033[0m $2"
+}
+
+# Usunięcie starych kontenerów i woluminów, jeśli istnieją
+docker rm -f $NGINX_CONTAINER || true
+docker volume rm -f $VOLUME || true
+
+docker volume create nginx_data
+
+info "NGINX" "Uruchamiam kontener Nginx"
+CONTAINER_ID=$(docker run -d --name $NGINX_CONTAINER -p 80:80 --volume $VOLUME:/usr/share/nginx/html nginx)
+
+info "MODYFIKACJA" "Wprowadzam własną zawartość do pliku index.html przu użyciu tymczasowego kontenera"
+docker run --rm -it -v $VOLUME:/mnt alpine sh -c "echo '<!DOCTYPE html>
+<html>
+  <head>
+    <title>Welcome to nginx!</title>
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+  </body>
+</html>' > /mnt/index.html"
+
+echo "Utworzono kontener o ID: $CONTAINER_ID"
